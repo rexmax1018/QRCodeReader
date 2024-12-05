13321efc2966c645cb86d8227d32048fbf2e9184
Rex Chen <rexmax1018@gmail.com>
2024-12-05 15:13:33 +0800

    更換使用 WebRTC 及 ZXing.js

--- QRCodeReader/Controllers/HomeController.cs
+++ QRCodeReader/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using QRCodeReader.Models;
 using System.Diagnostics;
 
@@ -18,15 +19,25 @@ namespace QRCodeReader.Controllers
             return View();
         }
 
-        public IActionResult Scan()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int? statusCode = null)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var viewModel = new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+
+            if (statusCode.HasValue)
+            {
+                // 根據狀態碼進行判斷，傳遞錯誤訊息
+                ViewData["ErrorMessage"] = statusCode switch
+                {
+                    404 => "Page not found.",
+                    403 => "You do not have permission to view this page.",
+                    401 => "You are not authorized.",
+                    500 => "An unexpected error occurred. Please try again later.",
+                    _ => "An unexpected error occurred."
+                };
+            }
+
+            return View(viewModel);
         }
     }
 }
--- QRCodeReader/Program.cs
+++ QRCodeReader/Program.cs
@@ -3,20 +3,54 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var mvcBuilder = builder.Services.AddRazorPages();
+
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    // 將所有例外狀況重導向到 /Home/Error
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+    // 設定 HSTS (僅用於生產環境)
     app.UseHsts();
 }
 
+// 啟用 HTTPS 強制重導向
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// 配置 StatusCodePages 來捕捉 404、403、401、500 等狀態碼，並重導向到自訂的 Error 頁面
+app.UseStatusCodePages(async context =>
+{
+    var response = context.HttpContext.Response;
+
+    // 根據不同的狀態碼重導向到指定的錯誤頁面
+    if (response.StatusCode == 404)
+    {
+        response.Redirect("/Home/Error?statusCode=404");
+    }
+    else if (response.StatusCode == 403)
+    {
+        response.Redirect("/Home/Error?statusCode=403");
+    }
+    else if (response.StatusCode == 401)
+    {
+        response.Redirect("/Home/Error?statusCode=401");
+    }
+    else if (response.StatusCode == 500)
+    {
+        response.Redirect("/Home/Error?statusCode=500");
+    }
+});
 
 app.UseAuthorization();
 
